1246fd86446fde778e6905d34e75ab95b5461764
jenpl1451 <jenpl1451@gmail.com>
2023-04-12 12:16:12 +0300

    added users table

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,14 +1,43 @@
 import { PrivateRoute } from "@/components/privateRoute";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import type { User } from "@/types/user";
+
+import { API_URL } from "@/constants";
 
 import { blueGrey } from "@mui/material/colors";
 
-
-import { Box, Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 export default function Admin() {
-  const [users, setUsers] = useState([]);
-  const [isLogin, setIsLoading] = useState(true);
+  const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const getListOfUsers = async () => {
+      const accessToken = localStorage.getItem("accessToken");
+
+      const result = await fetch(API_URL + "/users", {
+        headers: {
+          Authorization: accessToken as string,
+        },
+      });
+      if (!result.ok) return;
+      const usersJson = (await result.json()) as User[];
+      setUsers(usersJson);
+    };
+    getListOfUsers();
+  }, []);
   return (
     <PrivateRoute requireAdmin={true}>
       <Box
@@ -20,7 +49,48 @@ export default function Admin() {
           bgcolor: blueGrey[100],
         }}
       >
-        <Button variant="contained">Hello World</Button>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>NAME</TableCell>
+                <TableCell>USERNAME</TableCell>
+                <TableCell>ROLE</TableCell>
+                <TableCell align="right">ACTIONS</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users.map((user) => (
+                <TableRow
+                  key={user.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {user.id}
+                  </TableCell>
+                  <TableCell>{user.name}</TableCell>
+                  <TableCell>{user.username}</TableCell>
+                  <TableCell>{user.role}</TableCell>
+                  <TableCell align="right">
+                    <Stack
+                      direction="row"
+                      spacing={1}
+                      justifyContent="flex-end"
+                    >
+                      <IconButton>
+                        <EditIcon />
+                      </IconButton>
+                      <IconButton>
+                        <DeleteIcon />
+                      </IconButton>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </PrivateRoute>
   );
---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+
+
+export type User = {
+  id: string
+  username: string
+  password: string
+  role: string
+  name: string
+  createdAt: string
+  updatedAt: string
+}
